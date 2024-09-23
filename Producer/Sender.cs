6ff8481d059a48ff6819fa1a3877ad870f3c54e9
Guilleloper <pgaugusto24@gmail.com>
2024-09-23 17:08:49 -0500

    Agregar archivos de proyecto.

---
+++ Producer/Sender.cs
@@ -0,0 +1,24 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory
+{
+    HostName = "localhost"
+};
+
+using (var connection = factory.CreateConnection())
+{
+    using (var channel = connection.CreateModel())
+    {
+        channel.QueueDeclare("MetaQueue", false, false, false, null);
+
+        var message = "Bienvenido al curso de rabbitMQ y .NET";
+        var body = Encoding.UTF8.GetBytes(message);
+
+        channel.BasicPublish("", "MetaQueue", null, body);
+        Console.WriteLine("El mensaje fue enviado {0}", message);
+    }
+    Console.WriteLine("Preciona [enter ] para salir de la aplicacion");
+    Console.ReadLine();
+}
+
